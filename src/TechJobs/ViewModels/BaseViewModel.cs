011b0160aec0e4f7dca846a5929236660a480809
Jared Howard <j.riley.howard@gmail.com>
2018-06-16 14:20:21 -0500

    one commit to rule them all

---
+++ src/TechJobs/ViewModels/BaseViewModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using TechJobs.Models;
+
+namespace TechJobs.ViewModels
+{
+    public class BaseViewModel
+    {
+        public string Title { get; set; } = "";
+
+        public List<JobFieldType> Columns { get; set; }
+
+        public BaseViewModel()
+        {
+            // create all columns
+
+            Columns = new List<JobFieldType>();
+
+            foreach (JobFieldType enumVal in Enum.GetValues(typeof(JobFieldType)))
+            {
+                Columns.Add(enumVal);
+            }
+        }
+    }
+}
--- src/TechJobs/ViewModels/SearchJobsViewModel.cs
+++ src/TechJobs/ViewModels/SearchJobsViewModel.cs
@@ -1,15 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using TechJobs.Models;
 
 namespace TechJobs.ViewModels
 {
-    public class SearchJobsViewModel 
+    public class SearchJobsViewModel : BaseViewModel
     {
-        // TODO #7.1 - Extract members common to JobFieldsViewModel
-        // to BaseViewModel
-
         // The search results
         public List<Job> Jobs { get; set; }
 
@@ -19,25 +15,5 @@ namespace TechJobs.ViewModels
         // The search value
         [Display(Name = "Keyword:")]
         public string Value { get; set; } = "";
-
-        // All columns, for display
-        public List<JobFieldType> Columns { get; set; }
-
-        // View title
-        public string Title { get; set; } = "";
-
-        public SearchJobsViewModel()
-        {
-            // Populate the list of all columns
-
-            Columns = new List<JobFieldType>();
-
-            foreach (JobFieldType enumVal in Enum.GetValues(typeof(JobFieldType)))
-            {
-                Columns.Add(enumVal);
-            }
-
-
-        }
     }
 }
